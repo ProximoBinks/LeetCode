670a803eaa65e5110be820f63cb1b089d7e9c4cd
ProximoBinks <elliot.koh@student.adelaide.edu.au>
2022-11-08 16:27:35 +1030

    practice

--- test4.cpp
+++ test4.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main(){
-    string letters[2][2][2] = {
-  {
-    { "A", "B" },
-    { "C", "D" }
-  },
-  {
-    { "E", "F" },
-    { "G", "H" }
-  }
+class Car{
+    protected:
+    string _model;
+    int _year;
+    public:
+    Car(string model, int year): _model(model),_year(year){}
+    Car():Car("",0){}
+};
+class SportCar : public Car{
+    private:
+    int _top_speed;
+    public:
+    SportCar(string model, int year, int top_speed):Car(model,year),_top_speed(top_speed){}
+    SportCar():SportCar("",0,0){}
+    int get_top_speed(){return _top_speed;}
 };
 
-for (int i = 0; i < 2; i++) {
-  for (int j = 0; j < 2; j++) {
-    for (int k = 0; k < 2; k++) {
-      cout << letters[i][j][k] << "\n";
-    }
-  }
-}
+int main(){
+    Car* a_car = new SportCar("BMW", 2007, 250);
+    cout << a_car->get_top_speed()<<endl;
+    return 0;
 }
